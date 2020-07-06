9daca6050a6d7538a36386622a77d30ac529b53a
JINSUBIN <sbin95s@gmail.com>
2020-07-06 13:53:04 +0900

    20.07.06 memberDTO(rankString으로 변경)

--- final_project/src/main/java/com/KG/dao/AdminDAO.java
+++ final_project/src/main/java/com/KG/dao/AdminDAO.java
@@ -1,7 +1,6 @@
 package com.KG.dao;
 
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,24 +27,7 @@ public class AdminDAO {
 	}
 
 	// 회원 관리 페이지 > 회원 검색
-	public List<MemberDTO> searchList(String searchOption, String keyword) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("searchOption", searchOption);
-		if(searchOption.equals("m_rank")) {
-			if(keyword.contains("관") || keyword.contains("리") || keyword.contains("자")) {
-				map.put("keyword", "1");
-			} else if(keyword.contains("스") || keyword.contains("탭")) {
-				map.put("keyword", "2");
-			} else if(keyword.contains("일") || keyword.contains("반")) {
-				map.put("keyword", "3");
-			} else if(keyword.contains("대") || keyword.contains("기")) {
-				map.put("keyword", "4");
-			} else {
-				map.put("keyword", keyword);
-			}
-		} else {
-			map.put("keyword", keyword);
-		}
+	public List<MemberDTO> searchList(Map<String, Object> map) {
 		return sqlSession.selectList(namespace + ".searchList", map);
 	}
 
--- final_project/src/main/java/com/KG/service/admin/AdminSearchServImpl.java
+++ final_project/src/main/java/com/KG/service/admin/AdminSearchServImpl.java
@@ -1,5 +1,6 @@
 package com.KG.service.admin;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,29 @@ public class AdminSearchServImpl implements AdminService {
 
 	@Override
 	public void execute(Model model) {
-		Map<String, Object> map = model.asMap();
-		String searchOption = (String) map.get("searchOption");
-		String keyword = (String) map.get("keyword");
-		model.addAttribute("searchList", adminDAO.searchList(searchOption, keyword));
+		Map<String, Object> modelMap = model.asMap();
+		String searchOption = (String) modelMap.get("searchOption");
+		String keyword = (String) modelMap.get("keyword");
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("searchOption", searchOption);
+		if(searchOption.equals("m_rank")) {
+			if(keyword.contains("관") || keyword.contains("리") || keyword.contains("자")) {
+				map.put("keyword", "1");
+			} else if(keyword.contains("스") || keyword.contains("탭")) {
+				map.put("keyword", "2");
+			} else if(keyword.contains("일") || keyword.contains("반")) {
+				map.put("keyword", "3");
+			} else if(keyword.contains("대") || keyword.contains("기")) {
+				map.put("keyword", "4");
+			} else {
+				map.put("keyword", keyword);
+			}
+		} else {
+			map.put("keyword", keyword);
+		}
+
+		model.addAttribute("searchList", adminDAO.searchList(map));
 	}
 
 }
